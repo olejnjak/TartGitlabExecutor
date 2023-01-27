0ec48c7f2acffc35ec795548026a7f8b79d912d3
Jakub Olejnik <olejnjak@gmail.com>
2023-01-27 14:57:44 +0100

    ✨ Prepare clean command

--- Sources/ExecutorCore/Commands/Clean.swift
+++ Sources/ExecutorCore/Commands/Clean.swift
@@ -1,7 +1,18 @@
 import ArgumentParser
+import Foundation
+import System
 
 public struct Clean: AsyncParsableCommand {
     public init() {
         
+    }
+    
+    public func run() async throws {
+        let tart = TartService()
+        let system = System.shared
+        let vmID = ProcessInfo.processInfo.vmID
+        
+        try? tart.stop(vmName: vmID)
+        try? tart.delete(vmName: vmID)
     }
 }
